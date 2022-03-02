6f4f88f4d9c9972227cb87fa5799bfd03f74612d
vivvle <vivianvnle@gmail.com>
2022-03-01 17:17:10 -0800

    v1 of script for tidy tuesday 2022-03-01

---
+++ fueling_stations_20220301/fueling_station_20220301.R
@@ -0,0 +1,29 @@
+#### This script is for TidyTuesday 2022-03-01 ####
+#### Created by: Vivian Vy Le ####
+#### Updated on: 2022-03-01 ####
+
+#### load libraries ####
+library(tidyverse)
+library(here)
+library(palettetown)
+library(ggplot2)
+
+#### load data ####
+stations <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-01/stations.csv')
+mapdata<-map_data("usa")
+
+#### view data ####
+view(stations)
+
+#### data analysis ####
+fuel_data <-stations %>%
+  filter(COUNTRY == "US") %>%
+  select(X, Y, FUEL_TYPE_CODE, CITY, STATE, ZIP) %>%
+  rename(long = X, lat = Y)
+view(fuel_data)
+
+fuel_data %>%
+  ggplot() +
+  geom_polygon(aes(x = long, y = lat, fill = STATE, group = STATE)) + 
+  coord_fixed(2.0) +
+  guides(fill = "none")
