43f631d37a86ac327240d2270e1ffccbab58b2e0
vivvle <vivianvnle@gmail.com>
2022-03-12 23:20:32 -0800

    v2 of script and adding output

--- student_mobility_20220308/Scripts/student_mobility_v3_20220308.R
+++ student_mobility_20220308/Scripts/student_mobility_v3_20220308.R
@@ -1,13 +1,12 @@
 #### This script is for TidyTuesday 2022-03-08 ####
 #### Created by: Vivian Vy Le ####
-#### Updated on: 2022-03-10 ####
+#### Updated on: 2022-03-11 ####
 
 #### Load libraries ####
 library(tidyverse)
 library(here)
 library(palettetown)
 library(ggplot2)
-library(ggthemes)
 
 #### Load data ####
 erasmus <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-08/erasmus.csv')
@@ -18,21 +17,26 @@ view(erasmus)
 
 #### data analysis ####
 french_participants_data <-erasmus %>%
-  select(receiving_country_code, participants, participant_age, participant_gender, academic_year) %>%
-  filter(receiving_country_code == "FR",
-         participant_age < 40,
-         participant_age > 16) %>%
-  rename(country = receiving_country_code)
+  select(receiving_country_code, participants, participant_age, participant_gender, academic_year) %>% #picking out columns
+  filter(receiving_country_code == "FR", #filtering the country code to France
+         participant_age < 40, #filtering out participants to be under 40yo, there are multiple ages above 100?!?
+         participant_age > 16) %>% #filtering out participants to be over 16, again there are negative ages?!?
+  rename(country = receiving_country_code) #renaming the columns to be country, easier to reference back
 view(french_participants_data)
 
 #### plotting data ####
 french_participants_data %>%
   ggplot(aes(x = participant_gender, y = participant_age)) +
-  geom_boxplot() +
-  labs(title = "Students in Erasmus Program in France From 2014-2020",
+  geom_boxplot(aes(fill = participant_gender)) + #filling the box plot by gender
+  labs(title = "Students in Erasmus Program Going to France From 2014-2020",
        subtitle = "Students of Ages between 16 and 40",
        x = "Gender",
        y = "Age in Years",
+       fill = "Gender",
        caption = "TidyTuesday of Week 2022-03-08") +
-  scale_color_manual(values = pokepal(50)) +
-  facet_wrap(~academic_year)
+  scale_fill_manual(values = pokepal(151)) + #using mew as a color palette for the boxplot
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text = element_text(size = 10),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  facet_wrap(~academic_year) #allows to look at all academic years from the data
+ggsave(here("student_mobility_20220308", "Output","erasmus_france.png"), width = 6, height = 5)
