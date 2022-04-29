5d06979dc0216893dae17dd974a9e66022c31ad7
vivvle <vivianvnle@gmail.com>
2022-04-28 19:22:36 -0700

    script v2

--- crosswords_20220419/Scripts/crosswords_20220419.R
+++ crosswords_20220419/Scripts/crosswords_20220419.R
@@ -20,3 +20,39 @@ view(times)
 
 
 #### data analysis ####
+times_data <- times %>%
+  filter(!is.na(answer)) %>% #filtering out na's from data
+  mutate(answer = str_to_lower(answer)) %>% #changed words to lower case
+  count(answer, sort=TRUE) %>% #counting answers
+  mutate(answer = factor(answer)) %>% #changing characters to factors
+  filter(n > 25)
+view(times_data)
+
+dave_data <- big_dave %>%
+  filter(!is.na(answer)) %>% #filtering out na's from data 
+  mutate(answer = str_to_lower(answer)) %>% #changed words to lower case
+  count(answer, sort = TRUE) %>% #counting answers
+  mutate(answer = factor(answer)) %>% #changing characters to factors
+  filter(n > 58) 
+view(dave_data)
+
+
+#### plotting data 
+pal_b <- lacroix_palette("Berry", 21, type = "continuous")
+pal_o <- lacroix_palette("Orange", 21, type = "continuous")
+
+
+p1 <- times_data %>%
+  ggplot(aes(x = reorder(answer, n), y = n, fill = answer)) +
+  geom_col() +
+  labs(title = "Words Used as Answers Multiple Times in Times Crossword",
+       x = "Answers",
+       y = "Counts",
+       caption = "Source: TidyTuesday for Week 2022-04-19") +
+  theme(plot.title = element_text(hjust= 0.5,
+                                  size = 10),
+        axis.title = element_text(face = "bold")) +
+  scale_fill_manual(values = pal_b) +
+  coord_flip() +
+  guides(fill = FALSE)
+
